a3c9993a0fdbeba1aeb3f1a095ae8c5a3ffcd5f4
zeliha <104801285+zelihapala@users.noreply.github.com>
2023-09-01 00:39:19 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -52,3 +52,64 @@ document.getElementById('previous').addEventListener('click', (event) => {
   element_showImage3.setAttribute("src", imageUrlList[0]);
 
 });
+
+
+document.getElementById('summer').addEventListener('click', (event) => {
+  let element_listOfVacation = document.getElementById('listOfVacation');
+  element_listOfVacation.replaceChildren();
+  let new_li = document.createElement('li');
+  let new_a = document.createElement('a');
+  new_a.setAttribute("href", 'https://www.themarmarahotels.com/bodrum?utm_campaign=Google%20My%20Business&utm_source=Google&utm_medium=Local%20Search');
+  new_a.innerText = 'Antalya -Bodrum';
+
+  new_li.appendChild(new_a);
+
+  element_listOfVacation.appendChild(new_li);
+  let new_li2 = document.createElement('li');
+  let new_a2 = document.createElement('a');
+  new_a2.setAttribute("href", 'https://wyndhamgardenlara.com/en?gclid=Cj0KCQjwi7GnBhDXARIsAFLvH4n-vJqUTSroRPwDk74cYttmXy0nY0tvvn0_n4nkJRh8Yc1xVwmuusIaAi2pEALw_wcB');
+  new_a2.innerText = 'Antalya -Lara';
+
+  new_li2.appendChild(new_a2);
+
+  element_listOfVacation.appendChild(new_li2);
+  let new_li3 = document.createElement('li');
+  let new_a3 = document.createElement('a');
+  new_a3.setAttribute("href", 'https://www.delphinhotel.com/tr/delphin-be-grand-resort');
+  new_a3.innerText = 'Antalya-Kemer';
+
+  new_li3.appendChild(new_a3);
+
+  element_listOfVacation.appendChild(new_li3);
+
+});
+
+document.getElementById('winter').addEventListener('click', (event) => {
+  let element_listOfVacation2 = document.getElementById('listOfVacation');
+  element_listOfVacation2.replaceChildren();
+  let new_li4 = document.createElement('li');
+  let new_a4 = document.createElement('a');
+  new_a4.setAttribute("href", 'https://bofuludag.com/');
+  new_a4.innerText = 'Bursa-Uludag';
+
+  new_li4.appendChild(new_a4);
+
+  element_listOfVacation2.appendChild(new_li4);
+  let new_li5 = document.createElement('li');
+  let new_a5 = document.createElement('a');
+  new_a5.setAttribute("href", 'https://www.masalyamacotel.com/');
+  new_a5.innerText = 'Sivas -Hobbit Village';
+
+  new_li5.appendChild(new_a5);
+
+  element_listOfVacation2.appendChild(new_li5);
+  let new_li6 = document.createElement('li');
+  let new_a6 = document.createElement('a');
+  new_a6.setAttribute("href", 'https://www.gamirasu.com/');
+  new_a6.innerText = 'Nevsehir-Kapadokya';
+
+  new_li6.appendChild(new_a6);
+
+  element_listOfVacation2.appendChild(new_li6);
+
+});
